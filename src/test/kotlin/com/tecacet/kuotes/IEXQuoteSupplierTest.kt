c0fe4ace75c5d04df54d6774569031e061ab3046
algorythmist <Htig3er0s!>
2020-09-18 09:24:22 -0700

    fixed names

--- src/test/kotlin/com/tecacet/kuotes/IEXQuoteSupplierTest.kt
+++ src/test/kotlin/com/tecacet/kuotes/IEXQuoteSupplierTest.kt
@@ -8,19 +8,19 @@ import java.time.LocalDate
 class IEXQuoteSupplierTest {
 
     @Test
-    fun testReadQuotes() {
-        val quoteReader = IEXQuoteSupplier()
-        val quotes1m = quoteReader.getQuotes("NFLX", Range.ONE_MONTH)
+    fun getQuotes() {
+        val quoteSupplier = IEXQuoteSupplier()
+        val quotes1m = quoteSupplier.getQuotes("NFLX", Range.ONE_MONTH)
         assertTrue(quotes1m.size > 19)
 
-        val quotes1y = quoteReader.getQuotes("NFLX")
+        val quotes1y = quoteSupplier.getQuotes("NFLX")
         assertTrue(quotes1y.size > 250)
     }
 
     @Test
     fun testReadQuotesFromResource() {
-        val quoteReader = IEXQuoteSupplier(filename = "nflx_1y.json")
-        val quotes1m = quoteReader.getQuotes("NFLX")
+        val quoteSupplier = IEXQuoteSupplier(filename = "nflx_1y.json")
+        val quotes1m = quoteSupplier.getQuotes("NFLX")
         assertEquals(253, quotes1m.size)
         val quote1 = quotes1m[0]
         assertEquals(155.03, quote1.close)
@@ -29,9 +29,9 @@ class IEXQuoteSupplierTest {
     }
 
     @Test
-    fun testReadDividends() {
-        val quoteReader = IEXQuoteSupplier()
-        val dividends = quoteReader.getDividends("AAPL", Range.FIVE_YEARS)
+    fun getDividends() {
+        val quoteSupplier = IEXQuoteSupplier()
+        val dividends = quoteSupplier.getDividends("AAPL", Range.FIVE_YEARS)
         assertTrue(dividends.size > 0) //This API appears to be faulty
 
         val dividend = dividends[0]
@@ -40,9 +40,9 @@ class IEXQuoteSupplierTest {
     }
 
     @Test
-    fun testReadSplits() {
-        val quoteReader = IEXQuoteSupplier()
-        val splits = quoteReader.getSplits("AAPL", Range.FIVE_YEARS)
+    fun getSplits() {
+        val quoteSupplier = IEXQuoteSupplier()
+        val splits = quoteSupplier.getSplits("AAPL", Range.FIVE_YEARS)
         assertEquals(1, splits.size)
         val split = splits[0]
         assertEquals(4, split.toFactor)
